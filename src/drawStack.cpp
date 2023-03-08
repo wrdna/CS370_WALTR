da8873cbf893f5a0b0891625e4efee8712e6a2d6
Andrew Goldberg <goldbeaj@sunypoly.edu>
2023-03-07 19:03:32 -0500

    improved drawStack function

--- src/drawStack.cpp
+++ src/drawStack.cpp
@@ -2,46 +2,74 @@
 #include <iostream>
 #include <stack>
 
-void drawStack(std::stack<int> myStack) {
+using namespace std;
 
+void drawStack(stack<int> myStack);
+
+// int main()
+// {
+//     stack<int> s;
+//     for (int i = 0; i < 10; i++)
+//         s.push(i);
+//     drawStack(s);
+// }
+
+void drawStack(stack<int> myStack)
+{
+    
     int screenX = 320;
     int screenY = 240;
-    int bufferX = 50;
+    int bufferX = 10;
     int bufferY = 20;
-
+    
+    stack<int> set = myStack;
+    
     int size = myStack.size();
-
+    
     int barHeight = (screenY/size) - 10;
-
+    
+    bool ifprinted = false;
+    
     Tigr* screen = tigrWindow(screenX, screenY, (char*)"Your Stack!", 0);
-    while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE)) {
-
-        tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
-
-        tigrPrint(screen, tfont, 200, 150, tigrRGB(0,100,250), "Top of stack: %d", myStack.top());
-
-        bool test = false;
-
-        while(!test){
-            for(int i=0; i<size; i++) {
-                tigrRect(screen, bufferX, bufferY, 150, barHeight, tigrRGB(0,0,0));
-                tigrFillRect(screen, bufferX, bufferY, 150, barHeight, tigrRGB(250,250,0));
-                tigrPrint(screen, tfont, 200, (bufferY-barHeight/2), tigrRGB(0,100,250), "%d", myStack.top());
-                if(myStack.size() == 1) {
-                    tigrPrint(screen, tfont, 200, 200, tigrRGB(0,100,250), "Bottom of stack: %d", myStack.top());
+    
+    while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE))
+    {
+        
+        if (!ifprinted)
+        {
+            
+            tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
+            
+            tigrPrint(screen, tfont, 200, 150, tigrRGB(0,100,250), "Top of stack: %d", myStack.top());
+            
+            for(int i = 0; i < size; i++)
+            {
+                    tigrRect(screen, bufferX, bufferY, 150, barHeight, tigrRGB(0,0,0));
+                    tigrFillRect(screen, bufferX, bufferY, 150, barHeight, tigrRGB(250,250,0));
+                    tigrPrint(screen, tfont, 150, (bufferY-barHeight/50), tigrRGB(0,100,250), "%d", set.top());
+                    if(set.size() == 1)
+                    {
+                            tigrPrint(screen, tfont, 200, 200, tigrRGB(0,100,250), "Bottom of stack: %d", set.top());
+                    }
+                    
+                    set.pop();
+                    bufferY += barHeight;
+                    
+                    
+                    for(int j = -1; j < i; j++)
+                    {
+                        for(int g = -1; g < j; g++)
+                        {
+                                tigrUpdate(screen);
+                        }
+                    }
+                    
                 }
-                myStack.pop();
-                bufferY += barHeight;
-            }
-            test = true;
+            
+            ifprinted = true;
+            
         }
         tigrUpdate(screen);
     }
     tigrFree(screen);
 }
-
-
-
-
-
-
