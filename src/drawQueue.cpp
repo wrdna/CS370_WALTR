fa2827d5dac25fff077154265704fdcbc84fdc59
Andrew Goldberg <goldbeaj@sunypoly.edu>
2023-03-07 19:17:20 -0500

    fixed drawQueue

--- src/drawQueue.cpp
+++ src/drawQueue.cpp
@@ -2,18 +2,42 @@
 #include <iostream>
 #include <queue>
 
-void drawQueue(std::queue<int> myQueue) {
+using namespace std;
+
+void drawQueue(queue<int> myQueue);
+
+int main()
+{
+    
+    queue<int> q;
+    
+    for (int i = 0; i < 10; i++)
+        q.push(i);
+    
+    drawQueue(q);
+    
+}
+
+void drawQueue(queue<int> myQueue){
     int screenX = 320;
     int screenY = 240;
     int bufferX = 50;
     int bufferY = 20;
 
     int size = myQueue.size();
+    
+    queue<int> set = myQueue;
 
     int barHeight = (screenY/size) - 10;
+    
+    bool ifprinted = false;
 
     Tigr* screen = tigrWindow(screenX, screenY, (char*)"Your Queue!", 0);
-    while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE)) {
+    while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE))
+    {
+        
+        if (!ifprinted)
+        {
 
         tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
 
@@ -21,20 +45,34 @@ void drawQueue(std::queue<int> myQueue) {
 
         bool test = false;
 
-        while (!test) {
-            for(int i=0; i<size; i++) {
+            for(int i=0; i<size; i++)
+            {
                 tigrRect(screen, bufferX, bufferY, 50, barHeight, tigrRGB(0,0,0));
                 tigrFillRect(screen, bufferX, bufferY, 50, barHeight, tigrRGB(250,250,0));
-                tigrPrint(screen, tfont, 160, (bufferY-barHeight/2), tigrRGB(0,100,250), "%d", myQueue.front());
-                if(myQueue.size() == 1) {
-                    tigrPrint(screen, tfont, 200, 200, tigrRGB(0,100,250), "Bottom of Queue: %d", myQueue.front());
+                tigrPrint(screen, tfont, 160, (bufferY-barHeight/50), tigrRGB(0,100,250), "%d", set.front());
+                if(set.size() == 1)
+                {
+                    tigrPrint(screen, tfont, 200, 200, tigrRGB(0,100,250), "Bottom of Queue: %d", set.front());
                 }
-                myQueue.pop();
+                set.pop();
                 bufferY += barHeight;
+                for(int j = -1; j < i; j++)
+                {
+                    for(int g = -1; g < j; g++)
+                    {
+                        tigrUpdate(screen);
+                    }
+                }
+                
             }
-            test = true;
+
+            ifprinted = true;
+
         }
+        
         tigrUpdate(screen);
     }
     tigrFree(screen);
+
+    
 }
