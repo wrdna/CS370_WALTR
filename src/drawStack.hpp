b4a2abceb7e7494c1263916797df1eb561cb4bb0
Andrew Goldberg <goldbeaj@sunypoly.edu>
2023-03-22 15:34:33 -0400

    removed unecessary files

--- src/drawStack.hpp
+++
@@ -1,8 +0,0 @@
-#include <stack>
-
-#ifndef WALTR_DRAW_STACK
-#define WALTR_DRAW_STACK
-
-void drawStack(std::stack<int> myStack);
-
-#endif // WALTR_DRAW_STACK
--- src/drawVector.hpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-
-#ifndef WALTR_DRAW_VECTOR
-#define WALTR_DRAW_VECTOR
-
-void drawVector(std::vector<int> myVector);
-
-#endif // WALTR_DRAW_VECTOR
